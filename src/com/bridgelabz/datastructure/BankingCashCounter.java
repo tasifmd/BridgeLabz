5ae1ee3a79eea149c9ea05311e3105a727247e6e
“tasifmd” <tasifmd96@gmail.com>
2019-02-27 18:58:01 +0530

    mycommit

---
+++ src/com/bridgelabz/datastructure/BankingCashCounter.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.datastructure;
+
+import com.bridgelabz.utility.Utility;
+
+public class BankingCashCounter {
+
+	public static void main(String[] args) {
+		CustomQueue cq = new CustomQueue(20);
+		int count=0;
+		int cash_count=10000;
+		do {
+			System.out.println("Enter the choice");
+			System.out.println("1:Add  2:Check Cash  3:Exit");
+			int choice = Utility.inputInteger();
+			switch(choice){
+				case 1:
+					cq.insert(count++);
+					
+					System.out.println("Adding "+count+" person to the queue");
+					System.out.println("Do you want to: 1-Deposit 2-Withdraw");
+					int ch=Utility.inputInteger();
+					switch(ch){
+						case 1: 
+							System.out.println("Enter the amount to deposit");
+							int dep_amt=Utility.inputInteger();
+							cash_count+=dep_amt;
+							System.out.println(dep_amt+" is added");
+							break;
+						case 2:
+							System.out.println("Enter the amount to withdraw");
+							int withdraw=Utility.inputInteger();
+							if(withdraw>cash_count){
+								System.out.println("Cash of that amount is not available");
+							}
+							else{
+								cash_count-=withdraw;
+								System.out.println(withdraw+" is withdrawn");
+							}
+							break;
+						default:
+							System.out.println("Invalid choice");
+							System.exit(0);
+					}
+					cq.remove();
+					System.out.println("The person is removed");
+					break;
+				case 2: System.out.println("The amount of cash available is: "+cash_count);
+					break;
+				case 3: System.exit(0);
+					
+			}
+		}while(cash_count!=0);
+		System.out.println("Out of cash");
+	}
+
+}
--- src/com/bridgelabz/datastructure/Test.java
+++ src/com/bridgelabz/datastructure/Test.java
@@ -2,18 +2,11 @@ package com.bridgelabz.datastructure;
 public class Test {
 
 	public static void main(String[] args) {
-		CustomLinkedList li = new CustomLinkedList();
-		String s = "Tasif";
-		li.add(li, "Tasif");
-		li.add(li, "Mohammed");
-		li.add(li, "Perweez");
-		if(li.isAvailable(li.head, s)) {
-			System.out.println("Found");
-		}
-		else {
-			System.out.println("not Found");
-		}
-		li.printList(li);
+		CustomQueue q = new CustomQueue(20);
+		/*q.insert(10);
+		q.insert(20);
+		q.insert(30);*/
+		q.display();
 	}
 
 }
